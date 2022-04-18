186af68c8e6289177b4c0a8951a2d6ddac37ff73
Yuliia Nikhaienko <yuliia_nikhaienko@epam.com>
2022-04-18 19:50:41 +0300

    initial commit

---
+++ swiper/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+    effect: "slide",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
